ecdfe48a9a37e2ca2f186e56c87c81bccb232830
Piotr <charonek@gmail.com>
2018-11-09 08:06:24 +0100

    Add DataReader

--- src/app/Library.java
+++ src/app/Library.java
@@ -5,26 +5,23 @@ package app;
 //TODO: NEXT STEP IN APP
 
 import data.Book;
+import utils.DataReader;
 
 public class Library {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.5";
+        final String appName = "Biblioteka v0.6";
 
         Book[] books = new Book[1000];
-        books[0] = new Book("W pustyni i w puszczy", "Henryk Sienkiewicz", 2010,
-                296, "Greg", "9788373271890");
-        books[1] = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch",
-                2009, 352, "9788324620845", "Helion");
-        books[2] = new Book("SCJP Sun Certified Programmer for Java 6 Study Guide",
-                "Bert Bates, Katherine Sierra", 2008, 851, "McGraw-Hill Osborne Media",
-                "9780071591065");
+        DataReader dataReader = new DataReader();
 
         System.out.println(appName);
-        System.out.println("Książki dostępne w bibliotece:");
+        System.out.println("Wprowdź nową książkę: ");
+        books[0] = dataReader.readAndCreateBook();
+        books[1] = dataReader.readAndCreateBook();
+        dataReader.close();
+
         books[0].printInfo();
         books[1].printInfo();
-        books[2].printInfo();
-
         System.out.println("System może przechowywać do " + books.length + " książek");
     }
 
