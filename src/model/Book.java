4e85fbae41c80014aadb288694ba290618c93e76
Piotr <charonek@gmail.com>
2019-10-14 00:05:40 +0200

    Add Scanner and table v0.8

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,11 +1,7 @@
-package data;
-
-// Po modyfikatorach dostępu
-
-//TODO: NEXT STEP IN APP
+package model;
 
 public class Book {
-    // Pola
+    //Pola
     private String title;
     private String author;
     private int releaseDate;
@@ -13,21 +9,20 @@ public class Book {
     private String publisher;
     private String isbn;
 
-    // Konstruktory
+    //Konstruktory
     public Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+        this(title, author, releaseDate, pages, publisher);
+        this.isbn = isbn;
+    }
+
+    public Book(String title, String author, int releaseDate, int pages, String publisher) {
         this.title = title;
         this.author = author;
         this.releaseDate = releaseDate;
         this.pages = pages;
         this.publisher = publisher;
-        this.isbn = isbn;
     }
 
-    public Book(Book book) {
-        this(book.title, book.author, book.releaseDate, book.pages, book.publisher, book.isbn);
-    }
-
-    //Settery i Gettery
     public String getTitle() {
         return title;
     }
@@ -77,9 +72,10 @@ public class Book {
     }
 
     public void printInfo() {
-        String info = title + "; " + author + "; " + releaseDate + "; " + pages + "; " + publisher
-                + "; " + isbn;
+        String info = title + "; " + author + "; " + releaseDate + "; "
+                + pages + "; " + publisher + "; " + isbn;
         System.out.println(info);
     }
+
 
 }
