ea785e083838c45ea8b9188a7ca6c8776ea47db9
Piotr <charonek@gmail.com>
2019-10-20 17:15:10 +0200

    Add static v1.0

--- src/app/LibraryApp.java
+++ src/app/LibraryApp.java
@@ -1,9 +1,12 @@
 package app;
 
 public class LibraryApp {
+
+    private static final String APP_NAME = "Biblioteka v0.9";
+
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.9";
-        System.out.println(appName);
+
+        System.out.println(APP_NAME);
         LibraryControl libControl = new LibraryControl();
         libControl.controlLoop();
     }
